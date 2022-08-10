403d3b72456aeebc9fd9bc9378bd9ab7d1703d26
HanLuca <hanluca2022@gmail.com>
2022-08-10 15:48:32 +0900

    [B/0.05.1] Add Js

---
+++ Javascript/UI.js
@@ -0,0 +1,14 @@
+// function popDevAlert() {
+//     // if (Open === false) {
+//     //     let Open = true
+//     //     document.getElementById("dev-alert").style.display = "block";
+//     // } else {
+//     //     let Open = false
+//     //     document.getElementById("dev-alert").style.display = "none";
+//     // }
+//     document.getElementById("dev-alert").style.display = "block";
+// }
+
+// function cancelDevAlert() {
+//     document.getElementById("dev-alert").style.display = "none";
+// }
